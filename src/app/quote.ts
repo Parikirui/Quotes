8fe177ae1385af2bfb9bfb5129bf9f46fad769a8
Parikirui <isaac.kirui@student.moringaschool.com>
2021-08-23 00:00:18 +0300

    completed working on making the site to output completed date

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,9 +1,9 @@
 export class Quote {
 
 
-public showAuthorName: boolean;
-constructor(public id: number,public name: string,public description: string, public completeDate: Date){
-this.showAuthorName=false;
+// public showAuthorName: boolean;
+// constructor(public id: number,public name: string,public description: string, public completeDate: Date){
+// this.showAuthorName=false;
 
 
     
@@ -13,7 +13,7 @@ this.showAuthorName=false;
     last : string;
     showAuthorName: boolean;
 
-    constructor (id: number, name: string, quoteName: string, last: string) {
+    constructor (id: number, name: string, quoteName: string, last: string, public submitDate: Date) {
         this.id = id,
         this.name =name,
         this.quoteName = quoteName,
